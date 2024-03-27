bf852fa10eb045757e1be8c4ec1c1571ca8b17fd
Jenequaintech <jtho110@wgu.edu>
2024-03-27 11:00:40 -0500

    up

--- app.py
+++ app.py
@@ -56,3 +56,35 @@ def update_movie():
         movie['release_date'] = release_date
     
     return f"Updated movie: {movie}", 200
+
+@app.route('/movies', methods=['GET'])
+def get_movies():
+    return jsonify(movies)
+
+@app.route('/movies', methods=['POST'])
+def add_movie():
+    movie_data = request.json
+    new_id = str(max([int(k) for k in movies.keys()]) + 1)
+    movies[new_id] = movie_data
+    return jsonify({'message': 'Movie added successfully', 'movie': movies[new_id]})
+
+@app.route('/movies/<movie_id>', methods=['PUT'])
+def update_movie(movie_id):
+    if movie_id in movies:
+        movie_data = request.json
+        movies[movie_id].update(movie_data)
+        return jsonify({'message': 'Movie updated successfully', 'movie': movies[movie_id]})
+    else:
+        return jsonify({'message': 'Movie not found'}), 404
+
+@app.route('/movies/<movie_id>', methods=['DELETE'])
+def delete_movie(movie_id):
+    if movie_id in movies:
+        del movies[movie_id]
+        return jsonify({'message': 'Movie deleted successfully'})
+    else:
+        return jsonify({'message': 'Movie not found'}), 404
+    
+    @app.route ('actors', methods =[POST])
+    def get_actors():
+        return jsonify{actors}
